2b00c492b47d797ce6e6e4ff7fee36bac02489d0
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-29 11:57:55 +0200

    add notebook item page

---
+++ src/app/notebook/[noteId]/page.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { auth } from "@clerk/nextjs";
+import { and, eq } from "drizzle-orm";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+import TipTapEditor from "@/components/TipTapEditor";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { clerk } from "@/lib/db/clerk-server";
+import { $notes } from "@/lib/db/schema";
+import DeleteButton from "@/components/DeleteButton";
+
+type Props = {
+  params: {
+    noteId: string;
+  };
+};
+
+const NotebookPage = async ({ params: { noteId } }: Props) => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/dashboard");
+  }
+  const user = await clerk.users.getUser(userId);
+  const notes = await db
+    .select()
+    .from($notes)
+    .where(and(eq($notes.id, parseInt(noteId)), eq($notes.userId, userId)));
+
+  if (notes.length != 1) {
+    return redirect("/dashboard");
+  }
+  const note = notes[0];
+
+  return (
+    <div className="min-h-screen grainy p-8">
+      <div className="max-w-4x1 mx-auto">
+        <div className="border shadow-xl border-stone-200 rounded-lg p-4 flex items-center">
+          <Link href="/dashboard">
+            <Button className="bg-green-600" size="sm">
+              Back
+            </Button>
+          </Link>
+          <div className="w-3"></div>
+          <span className="font-semibold">
+            {user.firstName} {user.lastName}
+          </span>
+          <span className="inline-block mx-1">/</span>
+          <span className="text-semibold text-stone-500">{note.name}</span>
+          <div className="ml-auto">
+            <DeleteButton noteId={note.id} />
+          </div>
+        </div>
+
+        <div className="h-4"></div>
+        <div className="border-stone-200 shadow-1 border rounded-lg px-16 py-8 w-full">
+          <TipTapEditor note={note} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotebookPage;
