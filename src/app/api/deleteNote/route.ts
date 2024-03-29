2860afdb62723cd4d8b4576382df1b715ffd81e2
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-29 12:00:03 +0200

    add delete note api

---
+++ src/app/api/deleteNote/route.ts
@@ -0,0 +1,14 @@
+import { db } from "@/lib/db";
+import { $notes } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+
+export async function POST(req: Request) {
+  try {
+    const { noteId } = await req.json();
+    await db.delete($notes).where(eq($notes.id, parseInt(noteId)));
+    return new Response("Delete successfully", { status: 200 });
+  } catch (error) {
+    console.error("Failed to delete note", error);
+    return new Response("failed", { status: 500 });
+  }
+}
