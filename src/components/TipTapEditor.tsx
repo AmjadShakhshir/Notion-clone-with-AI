9a82bd7eee9f6011341b66f04390bba6f036f635
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-29 12:02:23 +0200

    add tiptap editor

---
+++ src/components/TipTapEditor.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useEffect } from "react";
+import { EditorContent, useEditor } from "@tiptap/react";
+import { StarterKit } from "@tiptap/starter-kit";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import Text from "@tiptap/extension-text";
+import { useCompletion } from "ai/react";
+
+import TipTapMenuBar from "./TipTapMenuBar";
+import { Button } from "./ui/button";
+import { useDebounce } from "@/lib/useDebounce";
+import { NoteType } from "@/lib/db/schema";
+
+type Props = { note: NoteType };
+
+const TipTapEditor = ({ note }: Props) => {
+  const [editorState, setEditorState] = React.useState(note.editorState || `<h1>${note.name}</h1>`);
+  const lastCompletion = React.useRef<string>("");
+
+  const { complete, completion } = useCompletion({
+    api: "/api/completion",
+  });
+  const saveNote = useMutation({
+    mutationFn: async () => {
+      const response = await axios.post("/api/saveNote", {
+        noteId: note.id,
+        editorState,
+      });
+      return response.data;
+    },
+  });
+
+  const customText = Text.extend({
+    addKeyboardShortcuts() {
+      return {
+        "Control-d": () => {
+          // take the last 30 words
+          const prompt = this.editor.getText().split(" ").slice(-30).join(" ");
+          complete(prompt);
+          return true;
+        },
+      };
+    },
+  });
+
+  const editor = useEditor({
+    autofocus: true,
+    extensions: [StarterKit, customText],
+    content: editorState,
+    onUpdate: ({ editor }) => {
+      setEditorState(editor.getHTML());
+    },
+  });
+
+  useEffect(() => {
+    if (!editor || !completion) return;
+    const diff = completion.slice(lastCompletion.current.length);
+    lastCompletion.current = completion;
+    editor.commands.insertContent(diff);
+  }, [completion, editor]);
+
+  const debouncedEditorState = useDebounce(editorState, 500);
+  useEffect(() => {
+    if (debouncedEditorState === "") return;
+    saveNote.mutate(undefined, {
+      onSuccess: (data) => {
+        console.log("success update!", data);
+      },
+      onError: (error) => {
+        console.error("error update!", error);
+      },
+    });
+  }, [debouncedEditorState]);
+
+  return (
+    <>
+      <div className="flex">
+        {editor && <TipTapMenuBar editor={editor} />}
+        <Button className="bg-green-600 ml-2" size="sm">
+          {saveNote.isPending ? "Saving..." : "Saved"}
+        </Button>
+      </div>
+      <div className="prose prose-sm w-full mt-4">
+        <EditorContent editor={editor} />
+      </div>
+      <div className="h-4"></div>
+      <span className="text-sm">
+        Tip: Press <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg">Ctrl + D</kbd> for AI autocomplete
+      </span>
+    </>
+  );
+};
+
+export default TipTapEditor;
