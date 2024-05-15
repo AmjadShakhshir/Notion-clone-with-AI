cca7cb64ffcf544d2ed4fb1e24e1e476b6f567de
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-05-16 01:13:37 +0300

    add custom message to error handler

--- src/helper/catchErrorHandler.ts
+++ src/helper/catchErrorHandler.ts
@@ -1,18 +1,18 @@
 // apiHandler.ts
-export function handleAsyncErrors(func: any) {
+export function handleAsyncErrors(func: any, customErrorMessage: string) {
   return async function (...args: any[]) {
     try {
       return await func(...args);
     } catch (error) {
-      console.error("Error completing text:", error);
+      console.error(`${customErrorMessage}:`, error);
 
       // Provide a user-friendly error message
       if ((error as any).response) {
         console.error("API Error Response:", (error as any).response.data);
-        return new Response("Sorry, I couldn't complete the text due to a server error. Please try again later.", { status: 500 });
+        return new Response(`Sorry, ${customErrorMessage} due to a server error. Please try again later.`, { status: 500 });
       }
 
-      return new Response("Sorry, I couldn't complete the text at this moment.", { status: 500 });
+      return new Response(`Sorry, ${customErrorMessage} at this moment.`, { status: 500 });
     }
   };
 }
